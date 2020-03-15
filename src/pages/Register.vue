84d134e702ddb07b0dd0b8db9bcf3bf9c83348a3
马登科 <1461531865@qq.com>
2020-03-15 19:57:59 +0800

    初始化提交

---
+++ src/pages/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="register">
+    <hm-header>注册</hm-header>
+    <hm-logo></hm-logo>
+
+    <hm-button @click="register">注册</hm-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    register() {
+      console.log("我要注册了");
+    }
+  }
+};
+</script>
+
+<style scoped>
+span {
+  color: red;
+}
+</style>
